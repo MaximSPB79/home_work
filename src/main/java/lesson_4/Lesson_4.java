cf659c13d29af856e6899d61c336bd81454989b4
MaximSPB79 <ozmaxim@gmail.com>
2021-12-12 12:32:29 +0300

    commit

---
+++ src/main/java/lesson_4/Lesson_4.java
@@ -0,0 +1,30 @@
+package lesson_4;
+
+public class Lesson_4 {
+    private String name;
+
+    Lesson_4 a = new Lesson_4();
+
+    public String getName() {
+
+        return name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(String.n);
+//        byte a = 127;
+//        a++;
+//        System.out.println(a);
+        /*
+        if (MAP[0][0] == symbol && MAP[0][1] == symbol && MAP[0][2] == symbol) return true;
+        if (MAP[1][0] == symbol && MAP[1][1] == symbol && MAP[1][2] == symbol) return true;
+        if (MAP[2][0] == symbol && MAP[2][1] == symbol && MAP[2][2] == symbol) return true;
+        if (MAP[0][0] == symbol && MAP[1][0] == symbol && MAP[2][0] == symbol) return true;
+        if (MAP[0][1] == symbol && MAP[1][1] == symbol && MAP[2][1] == symbol) return true;
+        if (MAP[0][2] == symbol && MAP[1][2] == symbol && MAP[2][2] == symbol) return true;
+        if (MAP[0][0] == symbol && MAP[1][1] == symbol && MAP[2][2] == symbol) return true;
+        if (MAP[2][0] == symbol && MAP[1][1] == symbol && MAP[0][2] == symbol) return true;
+        return false;
+    */
+    }
+}
